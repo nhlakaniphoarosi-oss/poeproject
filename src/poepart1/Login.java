50e19c1d330331bb1215b6313c9e4e843442e310
Student <Student@LabVM2049939.fdcasvtthnzezkfxen0crxwm4f.jnbx.internal.cloudapp.net>
2026-09-16 15:23:43 +0200

    Amended

--- src/poepart1/Login.java
+++ src/poepart1/Login.java
@@ -18,7 +18,7 @@ public class Login extends Registration {
 
     public String returnLoginStatus(boolean status, String firstName, String lastName) {
         if (status) {
-            return "Welcome " + getFirstName() + getLastName() + " It is great to see you again.";
+            return "Welcome " + getFirstName() + " " + getLastName() + " It is great to see you again.";
         } else {
             return "Username or password incorrect, please try again.";
         }
